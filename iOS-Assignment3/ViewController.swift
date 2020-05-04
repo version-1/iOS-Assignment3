9d21b849239f6cce4c9c3d50bc4577f8e2a5d35d
version-1 <j.onodera26@gmail.com>
2020-05-04 08:03:12 -0700

    first commit

--- iOS-Assignment3/ViewController.swift
+++ iOS-Assignment3/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let btn = UIBarButtonItem(title: "Top", style: .plain, target: self, action: #selector(toTop))
+        self.navigationItem.rightBarButtonItem = btn
+    }
+    
+    @objc func toTop () {
+        
     }
 
 
